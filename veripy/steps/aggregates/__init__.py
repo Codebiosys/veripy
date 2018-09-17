93b7b0ed77626391e030c0595f50fad68e121ba8
David Mote <davidmote@gmail.com>
2018-09-17 14:46:31 -0700

    Updates code to work after rebase

--- veripy/steps/aggregates/__init__.py
+++ veripy/steps/aggregates/__init__.py
@@ -1,5 +1,9 @@
 from .actions.auth import * # noqa
+from .checks.check_assertion import * # noqa
+from .checks.invert_check import * # noqa
 
 __all__ = [
-    'user_logged_in'
+    'user_logged_in',
+    'check_assertion',
+    'invert_sentence'
 ]
