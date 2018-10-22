49274e92db34b5ecd2f6bbd3eb4452691a71bb4d
Brian Schrader <brian@brianschrader.com>
2018-10-22 15:18:08 -0700

    Fixes to how select options are found.
    We've elected to not use the provided splinter `select()` method because it
    makes the assumption that the select element will have a `name` attribute which is unfortunately not a sound assumption. In this case, we use the select to get all of the options that are children of the select w/o caring about how the select is constructed.
    
    The only limitation of this method is that the select's SELECTOR in the
    configuration file *MUST* be an XPath. There's no easy way to do this
    for other selector types w/o lots of weird string manipluation.
    
    - Adds a new public method to the Page to allow users to fetch the selector
    properties by name.
    
    - Adds a new exceptions file and `ImproperlyConfigured` exception for
    configuration issues. (See Docs)

--- veripy/steps/forms/actions/select_option.py
+++ veripy/steps/forms/actions/select_option.py
@@ -2,6 +2,8 @@ import logging
 import splinter.exceptions
 from behave import when
 
+from veripy.exceptions import ImproperlyConfigured
+
 logger = logging.getLogger('forms')
 
 
@@ -21,7 +23,26 @@ def select_option_by(context, value, element_name):
     except context.page.ElementNotFound:
         raise AssertionError(f'The "{element_name}" was not found on the page.')
 
+    properties = context.page.get_element_properties(element_name)
+    by = properties.get('by')
+
+    if by != 'xpath':
+        raise ImproperlyConfigured(
+            f'Cannot select option for select with name "{element_name}". '
+            f'It looks like this element was found using "{by}". '
+            f'Currently only XPaths are supported for this method.'
+        )
+
+    selector = properties['selector']
+
     try:
-        element.select(value)
-    except (AttributeError, IndexError, splinter.exceptions.ElementDoesNotExist):
+        # NOTE: We've elected to not use the provided splinter `select()` method
+        # because it makes the assumption that the select element will have a
+        # `name` attribute which is unfortunately not a sound assumption. In
+        # this case, we just elect to get all of the options that are children
+        # of the select w/o caring about how the select is constructed.
+        element.find_by_xpath(
+            f'{selector}//option[@value="{value}"]'
+        )._element.click()
+    except splinter.exceptions.ElementDoesNotExist:
         raise AssertionError(f'"{element_name}" has no option with value "{value}".')
