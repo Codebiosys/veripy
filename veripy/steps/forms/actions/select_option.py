6aca1a5654974204093613c9682a4c072c3d120c
Brian Schrader <brian@brianschrader.com>
2018-10-23 12:50:24 -0700

    Adds new backwards-compatible select method tweaks.
    The select method, before attempting to select the item, checks if the
    element has a name. If it does then it can successfully apply the
    default splinter method. Otherwise the backup XPath method is tried.

--- veripy/steps/forms/actions/select_option.py
+++ veripy/steps/forms/actions/select_option.py
@@ -23,6 +23,28 @@ def select_option_by(context, value, element_name):
     except context.page.ElementNotFound:
         raise AssertionError(f'The "{element_name}" was not found on the page.')
 
+    if element._element.get_attribute('name'):
+        # Default to the preferred Splinter select method.
+        logger.debug(
+            'Select: "{element_name}" has a valid name. Preferring spliter select.'
+        )
+        try:
+            return element.select(value)
+        except (AttributeError, IndexError, splinter.exceptions.ElementDoesNotExist):
+            raise AssertionError(f'"{element_name}" has no option with value "{value}".')
+
+    # In this case, we haven't got a properly formatted <select> element so
+    # we'll attempt to backsolve for its values using XPaths. Splinter
+    # makes the assumption that the select element will have a `name` attribute
+    # which is unfortunately not a sound assumption. In this case, we just elect
+    # to get all of the options that are children of the select w/o caring about
+    # how the select is constructed.
+    #
+    # Note that this method only works if the element was selected by XPath.
+    logger.debug(
+        'Select: "{element_name}" has no name attribute. Trying XPath reversal select method.'
+    )
+
     properties = context.page.get_element_properties(element_name)
     by = properties.get('by')
 
@@ -36,13 +58,8 @@ def select_option_by(context, value, element_name):
     selector = properties['selector']
 
     try:
-        # NOTE: We've elected to not use the provided splinter `select()` method
-        # because it makes the assumption that the select element will have a
-        # `name` attribute which is unfortunately not a sound assumption. In
-        # this case, we just elect to get all of the options that are children
-        # of the select w/o caring about how the select is constructed.
         element.find_by_xpath(
             f'{selector}//option[@value="{value}"]'
         )._element.click()
-    except splinter.exceptions.ElementDoesNotExist:
+    except (AttributeError, IndexError, splinter.exceptions.ElementDoesNotExist):
         raise AssertionError(f'"{element_name}" has no option with value "{value}".')
